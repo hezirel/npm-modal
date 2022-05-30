e4fb661611d402608c4d108abfc6ab94c7469b99
hezirel <80560775+hezirel@users.noreply.github.com>
2022-05-30 20:41:48 +0200

    Modal module

---
+++ src/lib/Modal.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { PropTypes } from "prop-types";
+
+import "./Modal.css";
+
+const Modal = ({ handleState }) => {
+
+	const closeModal = (e) => {
+		e.preventDefault();
+		handleState(false);
+	};
+
+	return (
+		<div className="modalOverlay show">
+			<div className="confirmModal">
+				<h2>Successfully added subject to database</h2>
+				<div className="modalHeader">
+					<button onClick={closeModal}>Close this dialog</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+Modal.propTypes = {
+	handleState: PropTypes.func.isRequired,
+};
+
+export default Modal;
