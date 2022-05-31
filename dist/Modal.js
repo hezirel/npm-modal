9f05f50ba1cf6185eed86a9dd2c1383d7b511798
hezirel <80560775+hezirel@users.noreply.github.com>
2022-05-31 22:30:41 +0200

    Refactor with new modal npm package

--- dist/Modal.js
+++ dist/Modal.js
@@ -12,22 +12,10 @@ var _react = _interopRequireDefault(require("react"));
 require("./Modal.css");
 
 var Modal = function Modal(_ref) {
-  var handleState = _ref.handleState;
-
-  var closeModal = function closeModal(e) {
-    e.preventDefault();
-    handleState(false);
-  };
-
+  var children = _ref.children;
   return /*#__PURE__*/_react.default.createElement("div", {
-    className: "modalOverlay show"
-  }, /*#__PURE__*/_react.default.createElement("div", {
-    className: "confirmModal"
-  }, /*#__PURE__*/_react.default.createElement("h2", null, "Successfully added subject to database"), /*#__PURE__*/_react.default.createElement("div", {
-    className: "modalHeader"
-  }, /*#__PURE__*/_react.default.createElement("button", {
-    onClick: closeModal
-  }, "Close this dialog"))));
+    className: "modalOverlay"
+  }, children);
 };
 
 exports.Modal = Modal;
